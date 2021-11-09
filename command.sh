0b80bfefaf8881c5c6fadbb06aba579bd2fc078c
Zsolt Kovacs <smith.zsolt@gmail.com>
2021-11-09 09:26:19 +0100

    add npm start

--- command.sh
+++ command.sh
@@ -1,5 +1,10 @@
 NPM_TOKEN=${NPM_TOKEN}
+NPM_RUN=${NPM_RUN}
+if [ "$NPM_RUN" = "" ]; then
+  NPM_RUN="start"
+fi
 cd /usr/src/app
 npm config set //registry.npmjs.org/:_authToken $NPM_TOKEN
 npm install
-npm run start:docker
+npm run $NPM_RUN
+#npm run start:docker
